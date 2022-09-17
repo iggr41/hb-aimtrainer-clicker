56ee44cacf0b82b3fa1102e1a547ffa820930994
iggr41 <67658197+iggr41@users.noreply.github.com>
2022-09-17 16:21:51 -0300

    Update targetclicker.py

--- targetclicker.py
+++ targetclicker.py
@@ -2,12 +2,13 @@
 import pyautogui
 pyautogui.PAUSE = 0.01
 timer = 0
+conf = 0.26
 while timer <= 30:
-    targetLocation = pyautogui.locateCenterOnScreen('target.png', region=(530, 122, 869, 535), confidence=0.251)
+    targetLocation = pyautogui.locateCenterOnScreen('target.png', region=(530, 122, 869, 535), confidence=conf)
     if targetLocation is None:
         continue
     else:
-        targetLocation = pyautogui.locateCenterOnScreen('target.png', region=(530, 122, 869, 535), confidence=0.251)
+        targetLocation = pyautogui.locateCenterOnScreen('target.png', region=(530, 122, 869, 535), confidence=conf)
         pyautogui.click(targetLocation)
         print(  targetLocation)
         timer += 1
